e02b065f81e99e99c274636c3a9e431f64e3ae17
MikeGemini <mtrade@inbox.ru>
2020-02-21 13:09:54 +0500

    Create vcs repository

--- app/src/main/java/com/gemini_soft/noteitdown/SettingsActivity.java
+++ app/src/main/java/com/gemini_soft/noteitdown/SettingsActivity.java
@@ -18,6 +18,7 @@ public class SettingsActivity extends AppCompatActivity implements SeekBar.OnSee
    private SettingsHelper mSettingsHelper;
    private EditText mFileName;
 
+
    @Override
    protected void onCreate(Bundle savedInstanceState) {
       super.onCreate(savedInstanceState);
